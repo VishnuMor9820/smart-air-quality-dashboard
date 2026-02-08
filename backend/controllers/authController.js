c33df4f9ffb481d17738d7df2efe3b4548d5bccb
VishnuMor9820 <150892668+VishnuMor9820@users.noreply.github.com>
2026-02-08 10:47:35 +0530

    Create authController.js with user authentication functions

---
+++ backend/controllers/authController.js
@@ -0,0 +1,23 @@
+// authController.js
+
+// Function to register a new user
+const register = (req, res) => {
+    // Registration logic here
+};
+
+// Function to login a user
+const login = (req, res) => {
+    // Login logic here
+};
+
+// Function to get the current user
+const getCurrentUser = (req, res) => {
+    // Logic to fetch current user
+};
+
+// Function to update user preferences
+const updatePreferences = (req, res) => {
+    // Logic to update user preferences
+};
+
+module.exports = { register, login, getCurrentUser, updatePreferences };
