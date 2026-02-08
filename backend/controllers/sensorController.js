606c6ad090f7a89d26613db5016e12b06551fc24
VishnuMor9820 <150892668+VishnuMor9820@users.noreply.github.com>
2026-02-08 10:47:30 +0530

    Create sensorController.js file for managing sensor data and alerts

---
+++ backend/controllers/sensorController.js
@@ -0,0 +1,31 @@
+// backend/controllers/sensorController.js
+
+// Function to get the latest sensor data
+exports.getLatestSensorData = (req, res) => {
+    // Implementation here
+};
+
+// Function to get sensor data by date range
+exports.getSensorDataByDateRange = (req, res) => {
+    // Implementation here
+};
+
+// Function to get daily averages
+exports.getDailyAverages = (req, res) => {
+    // Implementation here
+};
+
+// Function to store sensor data
+exports.storeSensorData = (req, res) => {
+    // Implementation here
+};
+
+// Function to get alerts
+exports.getAlerts = (req, res) => {
+    // Implementation here
+};
+
+// Function to acknowledge alerts
+exports.acknowledgeAlert = (req, res) => {
+    // Implementation here
+};
