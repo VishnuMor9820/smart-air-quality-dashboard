4f5c7e7470c76ac2b721698d8d63b937a3ef6216
VishnuMor9820 <150892668+VishnuMor9820@users.noreply.github.com>
2026-02-08 10:48:02 +0530

    Create User.js file with Mongoose user schema including password hashing and comparePassword method

---
+++ backend/models/User.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// Define the user schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true });
+
+// Hash the password before saving the user model
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
